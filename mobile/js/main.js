fc89424334e16c2db908acfaa1a4b3601776fff7
jeonng <do2816@gmail.com>
2019-08-13 18:11:19 +0900

    190813 리뷰까지

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -3,4 +3,14 @@ $(function(){
       controls: false
    });
 
+   $('main .public ul').bxSlider({
+      controls: false,
+      pager: false,
+      moveSlides :1,
+      minSlides:2,
+      maxSlides:2,
+      slideWidth:163,
+      slideMargin:10
+   });
+
 });
