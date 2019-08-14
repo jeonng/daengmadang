ac06c153a8ec1949c6cf8aed1ab9415dbfa79617
jeonng <do2816@gmail.com>
2019-08-14 18:11:08 +0900

    190814

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -1,16 +1,25 @@
 $(function(){
+
+   //배너 슬라이드
    $('.banner ul').bxSlider({
       controls: false
    });
 
+   //top5
    $('main .public ul').bxSlider({
       controls: false,
       pager: false,
       moveSlides :1,
       minSlides:2,
-      maxSlides:2,
+      maxSlides:5,
       slideWidth:163,
-      slideMargin:10
+      slideMargin:10,
+      responsive: true
    });
 
+   //review 리뷰
+
+   $('main .review ul').bxSlider({
+      controls: false,
+   });
 });
