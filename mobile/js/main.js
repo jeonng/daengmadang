a853165e65d2ca1e765a1d46dc590181070ac274
jeonng <do2816@gmail.com>
2019-08-16 18:10:42 +0900

    190816 top 버튼해야함

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -22,4 +22,39 @@ $(function(){
    $('main .review ul').bxSlider({
       controls: false,
    });
+
+   //메뉴 햄버거 누르기 
+   var $bars = $('.top_icon .bars');
+
+   $bars.click(function(e){
+      e.preventDefault();
+      $('body').addClass('aside_open');
+
+   });
+   //x버튼 누르기 
+   var $aside_close = $('.close .times');
+
+   $aside_close.click(function(e){
+      e.preventDefault();
+      $('body').removeClass('aside_open');
+   });
+   //aside 메뉴 slideup
+   var $slidemenu = $('aside .main_menu ul'),
+       $aside_menu= $slidemenu.siblings('div');
+       
+
+   $slidemenu.slideUp();
+
+   $aside_menu.click(function(e){
+      e.preventDefault();
+      $(this).toggleClass('upDown');
+      $(this).find('a').toggleClass('click');
+      $(this).find('svg').toggleClass('click');
+      if($(this).hasClass('upDown')){
+         $(this).siblings('ul').slideDown(350);
+      }else{
+          $(this).siblings('ul').slideUp(350);
+      }
+   });   
+
 });
